7a8a9e730dd9e0f98896ab519784ee51272127f2
the-gamers45 <42207280+the-gamers45@users.noreply.github.com>
2022-02-18 19:39:20 +0300

    Update app.py

--- app.py
+++ app.py
@@ -20,64 +20,55 @@ sms = africastalking.SMS
 
 @app.route('/', methods=['POST', 'GET'])
 
-class Main:
-    def Greetings(self):
-        session_id = request.values.get("sessionId", None)
-        service_code = request.values.get("serviceCode", None)
-        phone_number = request.values.get("phoneNumber", None)
-        phone_number = []
-        phone_number.append(phone_number)
-        text = request.values.get("text")
 
-        if text == "":
-            now = maya.MayaDT.from_datetime(datetime.utcnow())
-            Time_zone = now.hour +3
+def Greetings(text):
+    session_id = request.values.get("sessionId", None)
+    service_code = request.values.get("serviceCode", None)
+    phone_number = request.values.get("phoneNumber", None)
+    phone_number = []
+    phone_number.append(phone_number)
+    text = request.values.get("text")
 
-            if 5<= Time_zone <12 :
-                Good_Morning="Good Morning"
-                variables.response =("CON {}"
-                                            "\nHow may i help you"
-                                            "\n  -Limit "
-                                            "\n  -Balance"
-                                            "\n  -Loan"
-                                            "\n  -Amount"
-                ).format(Good_Morning)
+    if text == "":
+        now = maya.MayaDT.from_datetime(datetime.utcnow())
+        Time_zone = now.hour +3
 
-            elif  12 <= Time_zone < 17 :
-                Good_Afternoon="Good Afternoon"
-                variables.response =("CON {}"
-                                            "\nHow may i help you"
-                                            "\n  -Limit "
-                                            "\n  -Balance"
-                                            "\n  -Loan"
-                                            "\n  -Amount"
-                        ).format(Good_Afternoon)
-            else:
-                Good_Evening="Good Evening"
-                variables.response =("CON {}"
-                                            "\nHow may i help you"
-                                            "\n  -Limit "
-                                            "\n  -Balance"
-                                            "\n  -Loan"
-                                            "\n  -Amount"
-                        ).format(Good_Evening)
+        if 5<= Time_zone <12 :
+            Good_Morning="Good Morning"
+            variables.response =("CON {}"
+                                        "\nHow may i help you"
+                                        "\n  -Limit "
+                                        "\n  -Balance"
+                                        "\n  -Loan"
+                                        "\n  -Amount"
+            ).format(Good_Morning)
 
-        self.Limmit()
-        return variables.response
+        elif  12 <= Time_zone < 17 :
+            Good_Afternoon="Good Afternoon"
+            variables.response =("CON {}"
+                                        "\nHow may i help you"
+                                        "\n  -Limit "
+                                        "\n  -Balance"
+                                        "\n  -Loan"
+                                        "\n  -Amount"
+                    ).format(Good_Afternoon)
+        else:
+            Good_Evening="Good Evening"
+            variables.response =("CON {}"
+                                        "\nHow may i help you"
+                                        "\n  -Limit "
+                                        "\n  -Balance"
+                                        "\n  -Loan"
+                                        "\n  -Amount"
+                    ).format(Good_Evening)
+        return
+    return variables.response
 
-    def Limmit(self):
-        text = request.values.get("text")
-        if text == "limit":
-            variables.response=("END Dear $first_name, your advance limit as at $date is KES $loan_limit.")
-        
-        return variables.response
-        
-            
-        
+
     
 
 
+    
+
 if __name__ == "__main__":
-    Object = Main()
-    Object.Greetings
     app.run(host="0.0.0.0", port=os.environ.get("PORT"))
