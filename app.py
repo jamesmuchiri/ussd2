ca91898d3a09d30c07e2a5666ef119e7a9a05704
the-gamers45 <42207280+the-gamers45@users.noreply.github.com>
2022-02-18 20:58:25 +0300

    Update app.py

--- app.py
+++ app.py
@@ -22,49 +22,52 @@ sms = africastalking.SMS
 @app.route('/', methods=['POST', 'GET'])
 
 
+class Main:
+    def Greetings(self,text):
+        session_id = request.values.get("sessionId", None)
+        service_code = request.values.get("serviceCode", None)
+        phone_number = request.values.get("phoneNumber", None)
+        phone_number = []
+        phone_number.append(phone_number)
+        self.text= request.values.get("text")
+
+        if text == "":
+            now = maya.MayaDT.from_datetime(datetime.utcnow())
+            Time_zone = now.hour +3
+
+            if 5<= Time_zone <12 :
+                Good_Morning="Good Morning"
+                variables.response =("CON {}"
+                                            "\nHow may i help you"
+                                            "\n  -Limit "
+                                            "\n  -Balance"
+                                            "\n  -Loan"
+                                            "\n  -Amount"
+                ).format(Good_Morning)
+
+            elif  12 <= Time_zone < 17 :
+                Good_Afternoon="Good Afternoon"
+                variables.response =("CON {}"
+                                            "\nHow may i help you"
+                                            "\n  -Limit "
+                                            "\n  -Balance"
+                                            "\n  -Loan"
+                                            "\n  -Amount"
+                        ).format(Good_Afternoon)
+            else:
+                Good_Evening="Good Evening"
+                variables.response =("CON {}"
+                                            "\nHow may i help you"
+                                            "\n  -Limit "
+                                            "\n  -Balance"
+                                            "\n  -Loan"
+                                            "\n  -Amount"
+                        ).format(Good_Evening)
+            return text
+        return variables.response
 
 
-def input(text):
-    text= request.values.get("text")
-    return text
-def Greetings(text):
-    if text == "":
-        now = maya.MayaDT.from_datetime(datetime.utcnow())
-        Time_zone = now.hour +3
-
-        if 5<= Time_zone <12 :
-            Good_Morning="Good Morning"
-            variables.response =("CON {}"
-                                        "\nHow may i help you"
-                                        "\n  -Limit "
-                                        "\n  -Balance"
-                                        "\n  -Loan"
-                                        "\n  -Amount"
-            ).format(Good_Morning)
-
-        elif  12 <= Time_zone < 17 :
-            Good_Afternoon="Good Afternoon"
-            variables.response =("CON {}"
-                                        "\nHow may i help you"
-                                        "\n  -Limit "
-                                        "\n  -Balance"
-                                        "\n  -Loan"
-                                        "\n  -Amount"
-                    ).format(Good_Afternoon)
-        else:
-            Good_Evening="Good Evening"
-            variables.response =("CON {}"
-                                        "\nHow may i help you"
-                                        "\n  -Limit "
-                                        "\n  -Balance"
-                                        "\n  -Loan"
-                                        "\n  -Amount"
-                    ).format(Good_Evening)
-        return text
-    return variables.response
-
-
-    
+        
 
 
     
