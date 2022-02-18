bd00e21cbd4c0bfd8d8fa6faec63f146c3d2af9a
the-gamers45 <42207280+the-gamers45@users.noreply.github.com>
2022-02-18 16:56:16 +0300

    Update app.py

--- app.py
+++ app.py
@@ -24,10 +24,11 @@ def callback():
     session_id = request.values.get("sessionId", None)
     service_code = request.values.get("serviceCode", None)
     phone_number = request.values.get("phoneNumber", None)
+    
     phone_number = []
     phone_number.append(phone_number)
 
-    def Greetings(text):
+    def Greetings():
         text = request.values.get("text")
         if text == "":
             now = maya.MayaDT.from_datetime(datetime.utcnow())
@@ -62,28 +63,27 @@ def callback():
                                     "\n  -Amount"
                 ).format(Good_Evening)
 
+        return Greetings(text) 
+
+    def Limit(text):
+        if text == "Limit" | text == "limit":
+            variables.response=("END Dear $first_name, your advance limit as at $date is KES $loan_limit.") 
+        return Limit
+    def Balance(text):
+        if text == "Balance" | text == "balance":
+            variables.response=("END Dear $first_name, your effective balance as at $date is KES $loan_balance.") 
+        return Balance
         
+    def Loan(text):
 
-        def Limit(text):
-            if text == "Limit" | text == "limit":
-                variables.response=("END Dear $first_name, your advance limit as at $date is KES $loan_limit.") 
-            return Limit
-        def Balance(text):
-            if text == "Balance" | text == "balance":
-                variables.response=("END Dear $first_name, your effective balance as at $date is KES $loan_balance.") 
-            return Balance
-            
-        def Loan(text):
+        if text == "Loan" | text == "loan":
+            variables.response=("END Dear $first_name, you qualify for a new loan. Please enter a loan value between 500 and $loan_limit") 
+        return Loan   
+    def Amount(text):
+        if text == "Amount" | text == "amount":
+            variables.response=("END Dear $first_name, you have selected KES XXXX, the loan advance will be processed shortl") 
+        return Amount  
 
-            if text == "Loan" | text == "loan":
-                variables.response=("END Dear $first_name, you qualify for a new loan. Please enter a loan value between 500 and $loan_limit") 
-            return Loan   
-        def Amount(text):
-            if text == "Amount" | text == "amount":
-                variables.response=("END Dear $first_name, you have selected KES XXXX, the loan advance will be processed shortl") 
-            return Amount  
-
-        return Greetings
 
     return variables.response
 
