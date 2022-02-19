cab84175cba4bb7bd62e669c36c533bf954f132a
the-gamers45 <42207280+the-gamers45@users.noreply.github.com>
2022-02-19 14:32:32 +0300

    Update app.py

--- app.py
+++ app.py
@@ -76,6 +76,7 @@ def Greetings():
         mycursor = db.cursor()
         mycursor.execute('''SELECT primary_phone FROM s_staff WHERE primary_phone = (%s)''', (variables.number,))
         checkNumber = mycursor.fetchall()
+        print(checkNumber)
 
         if (variables.number,) in checkNumber:
 
