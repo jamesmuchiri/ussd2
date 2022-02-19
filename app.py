c01dbea7af6381ee559b99746c9e2fec658b50c2
the-gamers45 <42207280+the-gamers45@users.noreply.github.com>
2022-02-19 16:58:27 +0300

    Update app.py

--- app.py
+++ app.py
@@ -33,10 +33,12 @@ def Greetings():
     session_id = request.values.get("sessionId", None)
     service_code = request.values.get("serviceCode", None)
 
-    phone_number =''
-    phone_number = request.values.get("phoneNumber")
+    phone = request.values.get("phoneNumber")
     
-    phone_number.replace(phone_number)
+    phone.replace(phone_number,phone)
+    
+    phone_number =""
+    
     text= request.values.get("text")
 
     if text == "":
