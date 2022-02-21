b43dc562eb7827b41132ab62a7794643a473e8eb
the-gamers45 <42207280+the-gamers45@users.noreply.github.com>
2022-02-21 14:18:52 +0300

    Update app.py

--- app.py
+++ app.py
@@ -25,37 +25,40 @@ def ussd_callback():
     text = request.values.get("text", "default")
     sms_phone_number = []
     sms_phone_number.append(phone_number)
+    kenya_time = datetime.datetime.utcnow() + datetime.timedelta(hours=3)
 
     #ussd logic
-    if text == "":
-        #main menu
-        response = "CON What would you like to do?\n"
-        response += "1. Check account details\n"
-        response += "2. Check phone number\n"
-        response += "3. Send me a cool message"
-    elif text == "1":
-        #sub menu 1
-        response = "CON What would you like to check on your account?\n"
-        response += "1. Account number"
-        response += "2. Account balance"
-    elif text == "2":
-        #sub menu 1
-        response = "END Your phone number is {}".format(phone_number)
-    elif text == "3":
-        try:
-            #sending the sms
-            sms_response = sms.send("Thank you for going through this tutorial", sms_phone_number)
-            print(sms_response)
-        except Exception as e:
-            #show us what went wrong
-            print(f"Houston, we have a problem: {e}")
-    elif text == "1*1":
-        #ussd menus are split using *
-        account_number = "1243324376742"
-        response = "END Your account number is {}".format(account_number)
-    elif text == "1*2":
-        account_balance = "100,000"
-        response = "END Your account balance is USD {}".format(account_balance)
+    
+    if text == "":  
+        
+        if 5<= kenya_time <12 :
+            Good_Morning="Good Morning"
+            response =("CON {}"
+                                        "\nHow may i help you"
+                                        "\n  -Limit "
+                                        "\n  -Balance"
+                                        "\n  -Loan"
+                                        "\n  -Amount"
+            ).format(Good_Morning)
+
+        elif  12 <= kenya_time < 17 :
+            Good_Afternoon="Good Afternoon"
+            response =("CON {}"
+                                        "\nHow may i help you"
+                                        "\n  -Limit "
+                                        "\n  -Balance"
+                                        "\n  -Loan"
+                                        "\n  -Amount"
+                    ).format(Good_Afternoon)
+        else:
+            Good_Evening="Good Evening"
+            response =("CON {}"
+                                        "\nHow may i help you"
+                                        "\n  -Limit "
+                                        "\n  -Balance"
+                                        "\n  -Loan"
+                                        "\n  -Amount"
+                    ).format(Good_Evening)
     else:
         response = "END Invalid input. Try again."
 
