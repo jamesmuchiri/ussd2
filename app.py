b72af32110ac508c4211f0fd3dbbf72fb62f1a70
the-gamers45 <42207280+the-gamers45@users.noreply.github.com>
2022-02-21 13:42:13 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,38 +8,64 @@ from datetime import datetime
 import datetime as dt
 from dateutil.parser import parse
 import mysql.connector
-from .menu import Menu
+from menu import Menu
+from flask import make_response
 app = Flask(__name__)
+
 
 username = "sandbox"
 api_key = "0f54c06969af94baa76c50efbcc1daaecb9b75f254d3388c85edfd9d21ff7ad0"
 africastalking.initialize(username, api_key)
 
 sms = africastalking.SMS
-db = mysql.connector.connect(
-    
-        host = "137.184.54.169",
-        user = "kaguius",
-        passwd = "U6xZfLn9A7Swc%P9",
-        database = "finabora",
-        autocommit = True,
-        port ="3306",
-    )
+
     
 @app.route('/', methods=['POST', 'GET'])
 
-class Airtime(Menu):
-    def Callback(self):
-        phone_number = request.values.get("phoneNumber","default")
-        text = request.values.get("text","default")
-        text_array = text.split("*")
-        user_response = text_array[len(text_array) - 1]
+
+def Callback():
+    phone_number = request.values.get("phoneNumber","default")
+    text = request.values.get("text","default")
+    text_array = text.split("*")
+    user_response = text_array[len(text_array) - 1]
+            
+            
+
+    if user_response == "":       
+        kenya_time = datetime.datetime.utcnow() + datetime.timedelta(hours=3)
+        if 5<= kenya_time <12 :
+            Good_Morning="Good Morning"
+            menu_text =("CON {}"
+                        "\nHow may i help you"
+                        "\n  -Limit "
+                        "\n  -Balance"
+                        "\n  -Loan"
+                        "\n  -Amount"
+            ).format(Good_Morning)
+        elif  12 <= kenya_time < 17 :
+            Good_Afternoon="Good Afternoon"
+            menu_text =("CON {}"
+                        "\nHow may i help you"
+                        "\n  -Limit "
+                        "\n  -Balance"
+                        "\n  -Loan"
+                         "\n  -Amount"
+                    ).format(Good_Afternoon)
+        else:
+            Good_Evening="Good Evening"
+            menu_text =("CON {}"
+                        "\nHow may i help you"
+                        "\n  -Limit "
+                        "\n  -Balance"
+                        "\n  -Loan"
+                        "\n  -Amount"
+                    ).format(Good_Evening)
+    return menu_text
+
+    
+      
+    
+
         
-        
-
-        if user_response == "":     
-            return self.home()
-
-
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=os.environ.get("PORT"))
